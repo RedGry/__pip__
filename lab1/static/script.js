fabc6330ecd0187bd82ffe07246aef1873ce0509
Sunny Capt <sunny.capt@tuta.io>
2019-08-28 03:06:56 +0300

    finished to write checking form values by js

--- lab1/static/script.js
+++ lab1/static/script.js
@@ -1,34 +1,81 @@
 "use strict";
 
-var yValues = [-4, -3, -2, -1, 0, 1, 2, 3, 4];
-var rValues = [1, 1.5, 2, 2.5, 3];
+const yValues = [-4, -3, -2, -1, 0, 1, 2, 3, 4];
+const rValues = [1, 1.5, 2, 2.5, 3];
+const xMax = 3;
+const xMin = -5;
 
-var yLine = document.createElement("tr");
+function onPageLoad() {
+    var xInput = document.getElementById("x-input");
+    xInput.style.backgroundColor = "white";
 
-var yResult = [];
-var rResult = [];
+    var yTable = document.getElementById("y-table");
+    var rTable = document.getElementById("r-table");
+    var yLine = document.createElement("tr");
 
-for (var i = 0; i < Math.max(yValues.length, rValues.length); i++) {
-    if (i < yValues.length) {
-        yLine.innerHTML += "<td>"+ yValues[i] +"<input type='checkbox' name='y" + yValues[i] + "'></td>";
-        if ((i+1) % 3 === 0) {
-            yResult.push(yLine);
-            yLine = document.createElement("tr");
+    var yResult = [];
+    var rResult = [];
+
+    for (var i = 0; i < Math.max(yValues.length, rValues.length); i++) {
+        if (i < yValues.length) {
+            yLine.innerHTML += "<td>" + yValues[i] + "<input type='checkbox' name='y" + yValues[i] + "'></td>";
+            if ((i + 1) % 3 === 0) {
+                yResult.push(yLine);
+                yLine = document.createElement("tr");
+            }
+        }
+        if (i < rValues.length) {
+            var tmpElem = document.createElement("tr");
+            tmpElem.innerHTML = "<td>" + rValues[i] + "<input type='checkbox' name='r" + rValues[i] + "'></td>";
+            rResult.push(tmpElem);
         }
     }
-    if (i < rValues.length) {
-        var tmpElem = document.createElement("tr");
-        tmpElem.innerHTML = "<td>"+ rValues[i] +"<input type='checkbox' name='r" + rValues[i] + "'></td>";
-        rResult.push(tmpElem);
-    }
+
+    yResult.forEach(function (value) {
+        yTable.append(value)
+    });
+    rResult.forEach(function (value) {
+        rTable.append(value)
+    });
+    document.getElementById("main").style.visibility = "visible";
 }
 
-var yTable = document.getElementById("y-table");
-var rTable = document.getElementById("r-table");
+function checkXValue(xInput) {
+    return (isNaN(Number(xInput.value)) || xInput.value < xMin || xInput.value > xMax);
+}
 
-yResult.forEach(function (value) {
-    yTable.append(value)
-});
-rResult.forEach(function (value) {
-    rTable.append(value)
-});
+function checkFormValues(lineArray, correctValues) {
+    for (var line in lineArray) {
+        for (var cell in line.children) {
+            if (!cell.children[0].checked)
+                continue
+            let v = cell.children[0].name;
+            v = Number(v.substring(1));
+            if (!(v in correctValues))
+                return true
+        }
+    }
+    return false;
+}
+
+function onEditX() {
+    var xInput = document.getElementById("x-input");
+    if (checkXValue(xInput))
+        xInput.style.backgroundColor = "red";
+    else
+        xInput.style.backgroundColor = "white";
+}
+
+function onSubmit() {
+    var xInput = document.getElementById("x-input");
+    var yArray = Array.from(document.getElementById("y-table").children);
+    var rArray = Array.from(document.getElementById("r-table").children);
+    yArray.shift();
+    rArray.shift();
+    var err = checkFormValues(yArray, yValues) || checkFormValues(rArray, rValues) || checkXValue(xInput);
+    if (err) {
+        event.preventDefault();
+        alert("wrong parameters!");
+    }
+    return !err;
+}
