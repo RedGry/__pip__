5e3380be4760e0ad2b7b6e8299107c3b2071d254
Karlen <sunny.capt@tuta.io>
2019-09-26 04:40:00 +0300

    fix clock

--- l1/static/script.js
+++ l1/static/script.js
@@ -110,6 +110,14 @@ function loadImage(url) {
     });
 }
 
+function clock() {
+    var date = new Date(),
+        hours = (date.getHours() < 10) ? '0' + date.getHours() : date.getHours(),
+        minutes = (date.getMinutes() < 10) ? '0' + date.getMinutes() : date.getMinutes(),
+        seconds = (date.getSeconds() < 10) ? '0' + date.getSeconds() : date.getSeconds();
+    document.getElementById('now').innerHTML = 'now: ' + hours + ':' + minutes + ':' + seconds;
+}
+
 const Kitty = function (queryUrl) {
     this.url = queryUrl;
 };
