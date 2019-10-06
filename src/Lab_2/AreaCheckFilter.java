ac3229ff1690b4f5cca14f873cc962e76501b9c8
gosha <sawin.georgij@yandex.ru>
2019-10-06 10:47:37 +0300

    Добавлено использование фильтров

---
+++ src/Lab_2/AreaCheckFilter.java
@@ -0,0 +1,54 @@
+package Lab_2;
+
+import javax.servlet.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+
+public class AreaCheckFilter implements Filter {
+
+    private int[] xValues = {-3, -2, -1, 0, 1, 2, 3, 4, 5};
+    private double[] rValues = {0, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5.5, 6, 6.5, 7.5, 8, 8.5, 9, 9.5, 10};
+
+    public void init(FilterConfig arg0) throws ServletException {}
+
+    public void doFilter(ServletRequest req, ServletResponse resp,
+                         FilterChain chain) throws IOException, ServletException {
+
+        PrintWriter out = resp.getWriter();
+
+        int x = Integer.parseInt(req.getParameter("x_h").trim());
+        double y = Double.parseDouble(req.getParameter("y_h"));
+        double r = Double.parseDouble(req.getParameter("r_h"));
+
+        if (validate(x, y, r))
+            chain.doFilter(req, resp);
+        else {
+            out.println(("<!DOCTYPE html>\n" +
+                    "<html>\n" +
+                    "<head>\n" +
+                    "\t<title>Тупо бан</title>\n" +
+                    "  \t<meta charset=\"utf-8\">\n" +
+                    "\t<link rel=\"stylesheet\" type=\"text/css\" href=\"css/handler_style.css\">\n" +
+                    "</head>\n" +
+                    "<body>" +
+                    "<img src=\"./img/ban.png\">" +
+                    "<script>" +
+                    "parent.ban();" +
+                    "</script>" +
+                    "</body> </html>"));
+        }
+
+    }
+
+    private boolean validate(int x, double y, double r) {
+        boolean checkX = Arrays.binarySearch(xValues, x) > -1;
+        boolean checkY = y >= -3 && y <= 5;
+        boolean checkR = Arrays.binarySearch(rValues, r) > -1;
+        return checkX && checkY && checkR;
+    }
+
+    public void destroy() {}
+
+}
+
--- src/Lab_2/AreaCheckServlet.java
+++ src/Lab_2/AreaCheckServlet.java
@@ -17,8 +17,6 @@ import java.util.List;
 public class AreaCheckServlet extends HttpServlet {
 
     private ServletConfig config;
-    private int[] xValues = {-3, -2, -1, 0, 1, 2, 3, 4, 5};
-    private double[] rValues = {0, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5.5, 6, 6.5, 7.5, 8, 8.5, 9, 9.5, 10};
 
     private PointsTableBean bean;
 
@@ -46,9 +44,16 @@ public class AreaCheckServlet extends HttpServlet {
             request.getSession().setAttribute("sessionBean", bean);
         }
 
+
         String reset = request.getParameter("reset");
 
-        boolean load = request.getParameter("load").equals("1");
+        boolean load = false;
+
+        try {
+            load = request.getParameter("load").equals("1");
+        } catch (NullPointerException e) {
+            request.getServletContext().getRequestDispatcher("/index.jsp").forward(request, response);
+        }
 
         if (!load) {
             try {
@@ -56,10 +61,7 @@ public class AreaCheckServlet extends HttpServlet {
                 double y = Double.parseDouble(request.getParameter("y_h"));
                 double r = Double.parseDouble(request.getParameter("r_h"));
 
-                Point p = null;
-
-                if (validate(x, y, r))
-                    p = new Point(x, y, r, bean.getN());
+                Point p = new Point(x, y, r, bean.getN());
 
                 bean.addPoint(p);
             } catch (Exception e) {
@@ -82,36 +84,17 @@ public class AreaCheckServlet extends HttpServlet {
                 "</head>\n" +
                 "<body>");
 
-        if (formTable(htmlResponse, reset)) {
-            htmlResponse.append("</body> </html>");
-            out.println(htmlResponse);
-        } else {
-            out.println(("<!DOCTYPE html>\n" +
-                    "<html>\n" +
-                    "<head>\n" +
-                    "\t<title>Тупо бан</title>\n" +
-                    "  \t<meta charset=\"utf-8\">\n" +
-                    "\t<link rel=\"stylesheet\" type=\"text/css\" href=\"css/handler_style.css\">\n" +
-                    "</head>\n" +
-                    "<body>" +
-                    "<img src=\"./img/ban.png\">" +
-                    "<script>" +
-                    "parent.ban();" +
-                    "</script>" +
-                    "</body> </html>"));
-        }
+        formTable(htmlResponse, reset);
+
+        htmlResponse.append("</body> </html>");
+        out.println(htmlResponse);
+
+        out.close();
 
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.sendRedirect("control");
-    }
-
-    private boolean validate(int x, double y, double r) {
-        boolean checkX = Arrays.binarySearch(xValues, x) > -1;
-        boolean checkY = y >= -3 && y <= 5;
-        boolean checkR = Arrays.binarySearch(rValues, r) > -1;
-        return checkX && checkY && checkR;
     }
 
     @SuppressWarnings("unchecked")
